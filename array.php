eddeecbaf9371eaf4e4c922e7385f1ad87de9a6d
AjrinNR <ajrinnr11@gmail.com>
2019-08-03 16:03:22 +0700

    array 1 dimensi dan looping

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+    $b = 'encep';
+    $a = [1,2,4,'Hai',100,300,500,800,'enjang',900,$b];
+    
+    for ($i=0; $i < count($a); $i++) { 
+        echo 'Index array ke-' . $i .' Dengan data ' . $a[$i] . '<br>';
+    }
+
+
+?>
