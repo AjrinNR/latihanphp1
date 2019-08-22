ae4462b3a16d48b09f05729620dab2bc50092556
AjrinNR <ajrinnr11@gmail.com>
2019-08-22 07:24:18 +0700

    array

--- array.php
+++ array.php
@@ -1,7 +1,7 @@
 <?php
      // array numeric
      $b = 'encep';
-     $a = [1,2,4,'Hai',100,300,500,800,'enjang',900,$b];
+     $a = [1,2,4,'Hai',100,300,500,80,'enjang',900,$b];
     
      for ($i=0; $i < count($a); $i++) { 
      echo 'Index array ke-' . $i .' Dengan data ' . $a[$i] . '<br>';
